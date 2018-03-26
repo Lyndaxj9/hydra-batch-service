5ed409a2307280b5903d436c9180e0c30e2c2105
Lynda Ademola <lyndaxj9@gmail.com>
2018-03-26 14:47:07 -0400

    added comments to controller

--- src/main/java/com/revature/hydra/batch/controller/BatchController.java
+++ src/main/java/com/revature/hydra/batch/controller/BatchController.java
@@ -35,43 +35,81 @@ public class BatchController {
 		this.batchService = batchService;
 	}
 	
+	/**
+	 * Retrieves a batch by name
+	 * @param name
+	 * @return ResponseEntity<Batch>
+	 */
 	@RequestMapping(value = "/one/batch/byname/{name}", method = RequestMethod.GET)
 	public ResponseEntity<Batch> getBatchByName(@PathVariable String name) {
 		return new ResponseEntity<>(batchService.getBatchByName(name), HttpStatus.OK);
 	}
 	
+	/**
+	 * Retrieves a batch by id
+	 * @param id
+	 * @return ResponseEntity<Batch>
+	 */
 	@RequestMapping(value = "/one/batch/byid/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Batch> getBatchByName(@PathVariable Integer id) {
 		return new ResponseEntity<>(batchService.getBatchById(id), HttpStatus.OK);
 	}
 	
+	/**
+	 * Retrieves all batches in order of start date
+	 * @return ResponseEntity<List<Batch>>
+	 */
 	@RequestMapping(value = "/all/batch/ordered", method = RequestMethod.GET)
 	public ResponseEntity<List<Batch>> getAllBatchesOrdered() {
 		return new ResponseEntity<>(batchService.getBatchesOrderedByDate(), HttpStatus.OK);
 	}
 	
+	/**
+	 * Retrieves all batches in a map
+	 * @return ResponseEntity<Map<Integer, Batch>>
+	 */
 	@RequestMapping(value = "/all/batch/mapped", method = RequestMethod.GET)
 	public ResponseEntity<Map<Integer, Batch>> getAllBatchesMapped() {
 		return new ResponseEntity<>(batchService.getBatchesMapped(), HttpStatus.OK);
 	}
 	
+	/**
+	 * Retrieves all batches in a set
+	 * @return ResponseEntity<HashSet<Batch>>
+	 */
 	@RequestMapping(value = "/all/batch/set", method = RequestMethod.GET)
 	public ResponseEntity<HashSet<Batch>> getAllBatchesSet() {
 		return new ResponseEntity<>(batchService.getBatchesSet(), HttpStatus.OK);
 	}
 	
+	/**
+	 * Create a new batch
+	 * @param batch
+	 * @return ResponseEntity<Void>
+	 */
 	@RequestMapping(value = "/add/batch", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> addBatch(@RequestBody Batch batch) {
 		batchService.addBatch(batch);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
+	/**
+	 * Update existing batch info
+	 * @param id
+	 * @param batch
+	 * @return ResponseEntity<Void>
+	 */
 	@RequestMapping(value = "/update/batch/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> updateBatchInfo(@PathVariable Integer id, @RequestBody Batch batch) {
 		batchService.updateBatch(id, batch);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
+	/**
+	 * Delete a batch by id
+	 * @param id
+	 * @return ResponseEntity<Void>
+	 */
 	@RequestMapping(value = "/delete/batch/{id}", method = RequestMethod.DELETE) 
 	public ResponseEntity<Void> deleteBatch(@PathVariable Integer id) {
 		batchService.deleteBatch(id);
